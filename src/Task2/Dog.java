2081e89ef3eecdb6e924626589badd74ebb3059a
Elisaveta Nickonova <liza.nickonova@mail.ru>
2022-09-04 10:54:04 +0300

    Второе задание выполнено

---
+++ src/Task2/Dog.java
@@ -0,0 +1,31 @@
+package Task2;
+
+public class Dog {
+    String  name;
+    int age;
+    public Dog (String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public String toString()
+    {
+        return (name + ", " + age);
+    }
+    public void toHumanAge()
+    {
+        System.out.println ("This dog's age in human age is " + age*7 + " years");
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName(String name){
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+}
--- src/Task2/TestDog.java
+++ src/Task2/TestDog.java
@@ -1,8 +1,28 @@
 package Task2;
+import java.util.*;
+
 
 public class TestDog {
     public static void main (String[] args) {
 
-        System.out.println("Test dog");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number of dogs: ");
+        int a = in.nextInt(), age;
+        String name;
+        Dog [] dogs = new Dog [a];
+        for (int i = 0; i < a; i++)
+        {
+            System.out.print("Input the name of the dog: ");
+            name = in.next();
+            System.out.print("Input the age of the dog: ");
+            age = in.nextInt();
+            dogs[i] = new Dog(name, age);
+        }
+        System.out.println("There are the dogs: ");
+        for (Dog d: dogs)
+        {
+            System.out.println(d);
+            d.toHumanAge();
+        }
     }
 }
